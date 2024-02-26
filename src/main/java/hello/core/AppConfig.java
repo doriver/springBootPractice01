937cddd5d0f63302bf6488db59f1a4fa2e46b904
doriver <tkdduq241@naver.com>
2024-02-26 16:24:59 +0900

    @Configuration과 싱글톤, 바이트코드 조작의 마법

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,21 +15,32 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
-@Configuration
-public class AppConfig {
 
+@Configuration // 없애본 결과도 봐보기
+public class AppConfig {
+	// @Bean memberRepository() --> new MemoryMemberRepository()
+    // @Bean memberService() --> memberRepository() --> new MemoryMemberRepository()
+    // @Bean orderService() --> memberRepository() --> new MemoryMemberRepository()
+	
+	// 스프링컨테이너 생성했을때 println되는 결과, @Configuration 없으면 call AppConfig.memberRepository가 총 3번 나옴
+//	call AppConfig.memberService
+//	call AppConfig.memberRepository
+//	call AppConfig.orderService
 	@Bean
 	public MemberService memberService() {
+		System.out.println("call AppConfig.memberService");
 		return new MemberServiceImpl(memberRepository());
 	}
 	
 	@Bean
 	public MemberRepository memberRepository() {
+		System.out.println("call AppConfig.memberRepository");
 		return new MemoryMemberRepository();
 	}
 	
 	@Bean
 	public OrderService orderService() {
+		System.out.println("call AppConfig.orderService");
 		return new OrderServiceImpl(memberRepository(), discountPolicy());
 	}
 	
