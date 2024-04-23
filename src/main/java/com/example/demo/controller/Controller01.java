55ae16f45c1349edba1e47ba5b4d31a003b3b734
doriver <tkdduq241@naver.com>
2024-04-23 21:21:28 +0900

    브라우저 주소창에 치면 그냥 데이터만 화면에 보이거나, html파일 랜더링 해서 보여줌 form제출했을땐 url이 바뀜, ajax요청 했을땐 url변경 없음 Controller의 반환값으론 크게 2가지 이다. ( html파일, json등의 데이터 ) 제이쿼리 적용함

--- src/main/java/com/example/demo/controller/Controller01.java
+++ src/main/java/com/example/demo/controller/Controller01.java
@@ -1,27 +1,54 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
-@RequestMapping("/01")
+@RequestMapping("/c01")
 @Slf4j
 public class Controller01 {
 	
-	@RequestMapping("/aa")
+	@GetMapping("/first")
 	public String first(Model model) {
 		model.addAttribute("message","Model addAttribute");
 		return "first";
 	}
 
+	/**
+	 * 데이터를 반환
+	 */
 	@ResponseBody
-	@RequestMapping("/asd")
-	public String aaa() {
-		log.info("========= asd =========");
-		return "hello";
+	@PostMapping("/aaa")
+	public String aaa(@RequestParam("nnn") String name) {
+		log.info("========= form recieve =========");
+		
+		log.info("데이터( 이름 ) : " + name);
+		
+		return name; 
+		// form제출했을땐, url이 c01/aaa로 바뀌면서그냥 화면이 name값만을 보여줘버림
+		// ajax요청 했을땐, url변경 없이, success부분 메소드만 뜸 
 	}
+
+	/**
+	 * HTML파일 반환
+	 */
+//	@PostMapping("/aaa")
+//	public String aaa(Model model, @RequestParam("nnn") String name) {
+//		log.info("========= form recieve =========");
+//		
+//		log.info("데이터( 이름 ) : " + name);
+//		model.addAttribute("message",name);
+//		
+//		return "second"; 
+//		// form제출했을땐, url이 c01/aaa로 바뀌면서 해당HTML파일 렌더링됨
+//		// ajax요청 했을땐, 거의 에러,  url변경 없이, HTML소스가 넘어가버림 
+//	}
 }
---
+++ src/main/java/com/example/demo/restController/RestController01.java
@@ -0,0 +1,25 @@
+package com.example.demo.restController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/rc01")
+@Slf4j
+public class RestController01 {
+	
+	@GetMapping("/rrss")
+	public String sdf() {
+		return "asdjflajdfl";
+	} // 이렇게 하면 그냥 화면이 String값만을 보여줘버림
+
+	@GetMapping("/rree")
+	public ResponseEntity<String> saadf() {
+		return ResponseEntity.ok().body("ResponseEntity.ok().body");
+	} // 화면에 ResponseEntity.ok().body 출력
+}
