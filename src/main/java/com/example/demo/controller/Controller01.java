deff62d0175d0b91f3023dd518d2bde49d212216
doriver <tkdduq241@naver.com>
2024-04-24 14:51:22 +0900

    데이터 여러개 넘겨주기 + map으로 반환

--- src/main/java/com/example/demo/controller/Controller01.java
+++ src/main/java/com/example/demo/controller/Controller01.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,12 +30,18 @@ public class Controller01 {
 	 */
 	@ResponseBody
 	@PostMapping("/aaa")
-	public String aaa(@RequestParam("nnn") String name) {
-		log.info("========= form recieve =========");
-		
+	public Map<String, String> aaa(@RequestParam("nnn") String name, @RequestParam("ppp") String password) {
+	
 		log.info("데이터( 이름 ) : " + name);
+		log.info("데이터( 비번 ) : " + password);
 		
-		return name; 
+		Map<String, String> map = new HashMap<>();
+		map.put("name", name);
+		map.put("password", password);
+		
+		return map; 
+		
+		// return name; 
 		// form제출했을땐, url이 c01/aaa로 바뀌면서그냥 화면이 name값만을 보여줘버림
 		// ajax요청 했을땐, url변경 없이, success부분 메소드만 뜸 
 	}
@@ -41,11 +50,14 @@ public class Controller01 {
 	 * HTML파일 반환
 	 */
 //	@PostMapping("/aaa")
-//	public String aaa(Model model, @RequestParam("nnn") String name) {
-//		log.info("========= form recieve =========");
+//	public String aaa( Model model
+//			, @RequestParam("nnn") String name, @RequestParam("ppp") String password) {
 //		
 //		log.info("데이터( 이름 ) : " + name);
-//		model.addAttribute("message",name);
+//		log.info("데이터( 비번 ) : " + password);
+//		
+//		model.addAttribute("nm",name);
+//		model.addAttribute("pm",password);
 //		
 //		return "second"; 
 //		// form제출했을땐, url이 c01/aaa로 바뀌면서 해당HTML파일 렌더링됨
