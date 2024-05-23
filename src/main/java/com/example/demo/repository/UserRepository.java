78a4676d371b9f2cf782dc27752770e9bed50d97
doriver <tkdduq241@naver.com>
2024-05-23 17:27:45 +0900

    커스텀 findBy(엔티티 필드네임)Name

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	List<User> findByUsername(String username);
+	List<User> findByEmail(String email);
+	
 }
--- src/test/java/com/example/demo/repo/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repo/UserRepositoryTest.java
@@ -17,63 +17,37 @@ import org.springframework.test.context.ActiveProfiles;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
+/**
+ * 테스트 해보는 곳
+ */
+
 //@SpringBootTest // 이거 없으면 안됨 , 다른 방법도 있을듯?
 
 @DataJpaTest// 이거 하나만 있으면 안됨
-//@ActiveProfiles("local") 이거 없어도 됐음
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 /* @AutoConfigureTestDataBase : 데이터 소스를 어떤 걸로 사용할지에 대한 설정
 Replace.Any : 기본적으로 내장된 데이터소스를 사용
 Replace.NONE : @ActiveProfiles 기준으로 프로파일이 설정됨
  */
 @Rollback(false)
+
+//@ActiveProfiles("local") 이거 없어도 됐음
 public class UserRepositoryTest {
+	
 	@Autowired 
 	UserRepository userRepository;
 	
 	@Test
-	public void findById() {
-		// given
-		Long id = 1L;
-		
-		// when
-		Optional<User> ou = userRepository.findById(id);
-		
-		// then
-		assertEquals(ou.get().getEmail(), "naver.com");
-	}
-	
-	@Test
-	public void findAll() {
-		List<User> li = userRepository.findAll();
-		System.out.println(li);
-	}
-
-	@Test
-	public void save() {
-		User us = new User();
-		us.setUsername("asdad");
-		us.setEmail("gooom");
-		
-		User inserted = userRepository.save(us);
-		
-		Optional<User> ou = userRepository.findById(inserted.getId());
-		
-		System.out.println(inserted);
-		System.out.println(ou);
-		
-		assertThat(inserted).isEqualTo(ou.get());
-	}
-	@Test
-	public void ss() {
-		
-	}
-	@Test
-	public void sa() {
-		
-	}
-	@Test
 	public void se() {
 		
 	}
+	@Test
+	public void sea() {
+		
+	}
+	@Test
+	public void see() {
+		
+	}
+	
 }
