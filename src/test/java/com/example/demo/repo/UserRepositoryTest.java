e2e83319abcf5c87504bdafbc3ac7dbe6c04edcf
doriver <tkdduq241@naver.com>
2024-05-23 15:06:03 +0900

    @DataJpaTest 로 테스트 성공 , 몇가지 추가코드 필요( @ , profiles.active )

--- src/test/java/com/example/demo/repo/UserRepositoryTest.java
+++ src/test/java/com/example/demo/repo/UserRepositoryTest.java
@@ -6,12 +6,23 @@ import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
-@SpringBootTest // 이거 없으면 안됨 , 다른 방법도 있을듯?
+//@SpringBootTest // 이거 없으면 안됨 , 다른 방법도 있을듯?
+
+@DataJpaTest// 이거 하나만 있으면 안됨
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+/* @AutoConfigureTestDataBase : 데이터 소스를 어떤 걸로 사용할지에 대한 설정
+Replace.Any : 기본적으로 내장된 데이터소스를 사용
+Replace.NONE : @ActiveProfiles 기준으로 프로파일이 설정됨
+ */
 public class UserRepositoryTest {
 	@Autowired 
 	UserRepository userRepository;
