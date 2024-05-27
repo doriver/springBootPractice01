7b0796d7cc9e728920930bf8b3a5b5e163a943d7
doriver <tkdduq241@naver.com>
2024-05-27 19:32:04 +0900

    쿠키 삭제 확인

--- src/main/java/com/example/demo/web/login/cookie/CookieController.java
+++ src/main/java/com/example/demo/web/login/cookie/CookieController.java
@@ -30,10 +30,11 @@ public class CookieController {
 	@GetMapping("/cookieView")
 	public String ckv( @CookieValue(name="memberId", required=false) Long memberId
 			, Model model ) { // @CookieValue로 요청안에 있는 쿠키 조회함
-		
-		if (memberId == 1) {
+		if ( memberId != null) {
+			if (memberId == 1) {
 //			model.addAttribute("member", "memberId :" + memberId);
-			return "cookie/hasCookie";
+				return "cookie/hasCookie";
+			}			
 		}
 		
 		return "cookie/noCookie";
@@ -43,6 +44,17 @@ public class CookieController {
 	@ResponseBody
 	public String aa(@CookieValue(name="memberId", required=false) Long memberId) {
 		return "memberId :" + memberId;
-	}
+	} // 쿠키 넣어준 상태에서, ajax로 요청했을때도 쿠키 들어있는거 확인함
 	
+	@GetMapping("/deleteCookie")
+	@ResponseBody
+	public String cookieOut(HttpServletResponse response) {
+		/*
+		 * 쿠키 삭제
+		 */
+		Cookie cookie = new Cookie("memberId", null);
+		cookie.setMaxAge(0); // max-age 0이나 음수지정하면 쿠키삭제 됨
+		response.addCookie(cookie);
+		return "";
+	}
 }
