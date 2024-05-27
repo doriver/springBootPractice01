f5a2f7ca1c3c6b0172081305f399328fb42c8246
doriver <tkdduq241@naver.com>
2024-05-27 15:16:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demo/SessionCookie01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionCookie01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionCookie01Application.class, args);
+	}
+
+}
