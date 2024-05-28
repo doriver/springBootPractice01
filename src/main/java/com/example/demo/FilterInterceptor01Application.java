f5ebd5d054640f10a1413e6820fb192e5201d8be
doriver <tkdduq241@naver.com>
2024-05-28 20:46:31 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demo/FilterInterceptor01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterInterceptor01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterInterceptor01Application.class, args);
+	}
+
+}
