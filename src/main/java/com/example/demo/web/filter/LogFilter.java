ac0aaeb890a44950d56f145bb6886b6d2a12b91b
doriver <tkdduq241@naver.com>
2024-06-03 19:03:50 +0900

    첫번째 필터는 try ~ finally 의 finally 부분이 , 요청에대한 처리가 끝난후 응답전에 실행됨

--- src/main/java/com/example/demo/web/filter/LogFilter.java
+++ src/main/java/com/example/demo/web/filter/LogFilter.java
@@ -11,7 +11,9 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-
+/**		첫번째 필터
+ * 
+ */
 @Slf4j
 public class LogFilter implements Filter {
 	// 필터를 사용하려면 필터 인터페이스를 구현해야 한다.
@@ -41,7 +43,9 @@ public class LogFilter implements Filter {
 			//  다음 필터가 있으면 필터를 호출하고, 필터가 없으면 서블릿을 호출한다. 만약 이로직을 호출하지 않으면 다음 단계로 진행되지 않는다.
 		} catch(Exception e) {
 			throw e; // 톰캣까지 예외를 보내줘야함
-		} finally {
+			
+		} finally { // 요청에대한 처리가 끝난후, 응답전에 실행됨
+			
 			log.info("Response [{}][{}]", ip, requestURI); // 2번째 필터( LoginCheckFilter ) finally까지 수행되고나서 수행됨
 			// LoginCheckFilter에서 DispatcherServlet까지 가는경우, 컨트롤러 메서드 호출 후 이루어짐, 응답직전인듯?
 			// 그냥 응답 직전에 수행되는듯??? 
