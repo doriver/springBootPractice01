d964c11f585664d41f18cd5c70e0cdb87165e226
doriver <tkdduq241@naver.com>
2024-06-14 17:52:41 +0900

    브랜치 테스트

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity // Spring Security를 활성화함 
+@EnableWebSecurity // Spring Security를 활성화함  
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
