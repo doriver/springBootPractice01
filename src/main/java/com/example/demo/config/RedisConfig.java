ad07c30056f9c5f58fb5d0702ee6c2ad011e533f
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-06-26 19:06:59 +0900

    Redis의 특정 데이터베이스(index 5)를 사용위한 설정

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -12,7 +13,9 @@ public class RedisConfig {
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory();
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+	    config.setDatabase(5); // 5번 DB 설정
+		return new LettuceConnectionFactory(config);
 	}
 	
 	@Bean
