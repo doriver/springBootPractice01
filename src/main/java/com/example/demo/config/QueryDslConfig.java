ee31a915d484e7d15fe60582f24f4f39af45092f
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-07-04 16:59:51 +0900

    일단 성공한거 설명

--- src/main/java/com/example/demo/config/QueryDslConfig.java
+++ src/main/java/com/example/demo/config/QueryDslConfig.java
@@ -20,3 +20,10 @@ public class QueryDslConfig {
     	return new JPAQueryFactory(entityManager);
     }
 }
+/**
+ * Q클래스 : Gradle Tasks에서 해당프로젝트-build-build해야 생김, build/generated/sources/annotationProcessor/java/main 디렉토리에 생성됨
+ * 
+ * Q클래스가 있는 build/generated/sources/annotationProcessor/java/main를 Source폴더에 등록해줘야함
+ * ( 프로젝트 - Properties - Java Build Path - Source - Add Folder )
+ */
+
