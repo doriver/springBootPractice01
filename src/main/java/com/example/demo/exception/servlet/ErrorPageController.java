6be933b1a941b2ae1cdbd1dd8ee888796f55d19a
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-08-06 20:13:15 +0900

    ** 오류 화면 커스텀 해서 제공 ** WebServerFactoryCustomizer 로 구현했을때 ConfigurableWebServerFactory에 addErrorPages 등록해노면 오류 발생하면 was는 그거 인식한뒤, 다시 ErrorPage에 있는 Controller경로를 호출함

---
+++ src/main/java/com/example/demo/exception/servlet/ErrorPageController.java
@@ -0,0 +1,26 @@
+package com.example.demo.exception.servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/error-page")
+public class ErrorPageController {
+
+	@RequestMapping("/404")
+	public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
+		log.info("errorPage 404");
+		return "error-page/404";
+	}
+
+	@RequestMapping("/500")
+	public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
+		log.info("errorPage 500");
+		return "error-page/500";
+	}
+}
