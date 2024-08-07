103260eba924fe5f1bab68944015746be7250abc
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-08-07 20:07:53 +0900

    API 예외 처리( 서블릿 에서 ) 기존만들어져 있는 ErrorPage구조에서 해봄, MediaType으로 구별해주고 그냥컨트롤러인데 ResponseEntity로 return해줘서

--- src/main/java/com/example/demo/exception/servlet/ErrorPageController.java
+++ src/main/java/com/example/demo/exception/servlet/ErrorPageController.java
@@ -1,8 +1,15 @@
 package com.example.demo.exception.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +39,19 @@ public class ErrorPageController {
 		log.info("errorPage 500");
 		printErrorInfo(request);
 		return "error-page/500";
+	}
+	
+	@RequestMapping(value="/500", produces=MediaType.APPLICATION_JSON_VALUE) // HTTP Header에 Accept가 application/json 이여야 함
+	public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
+		log.info("API errorPage 500");
+		
+		Map<String,Object> result = new HashMap<>();
+		Exception ex = (Exception)request.getAttribute(ERROR_EXCEPTION);
+		result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+		result.put("message", ex.getMessage());
+		
+		Integer statusCode = (Integer)request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
 	}
 	
 	private void printErrorInfo(HttpServletRequest request) {
