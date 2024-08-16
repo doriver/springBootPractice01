cfce7bbadb31ea04c99f356efc6dff2f2b252c92
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-08-16 14:14:48 +0900

    BeanValidation 첫 사용

---
+++ src/main/java/com/example/demo/validation/Item.java
@@ -0,0 +1,35 @@
+package com.example.demo.validation;
+
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class Item {
+
+	private Long id;
+
+	@NotBlank
+	private String itemName;
+	
+	@NotNull
+	@Range(min = 1000, max = 100000)
+	private Integer price;
+	
+	@NotNull
+	@Max(9999)
+	private Integer quantity;
+	
+	public Item() {
+		
+	}
+	
+	public Item(String itemName, Integer price, Integer quantity) {
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+}
