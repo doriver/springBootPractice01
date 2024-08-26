c0c62f30008cb5c343c616f15a7fe1e94ffec384
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-08-26 18:16:48 +0900

    조금 함

--- src/main/java/com/example/demo/crud/basic/Create.java
+++ src/main/java/com/example/demo/crud/basic/Create.java
@@ -1,9 +1,10 @@
 package com.example.demo.crud.basic;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +20,13 @@ public class Create {
 	
 	private final UserRepository userRepository;
 	
-	@PostMapping()
-	public void create() {
-		
+	@PostMapping("")
+	public User create(@RequestBody Map<String, String> data) {
+		User user = User.builder()
+				.email(data.get("email")).username(data.get("username"))
+				.build();
+		User savedUser = userRepository.save(user);
+		return savedUser;
 	}
 
 	public void aa() {
