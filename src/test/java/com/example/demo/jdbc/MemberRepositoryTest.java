522753423e05f55b3925bbaa9e23d3d3e75439b9
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-10-22 21:34:51 +0900

    JDBC로 DB에서 select성공

--- src/test/java/com/example/demo/jdbc/MemberRepositoryTest.java
+++ src/test/java/com/example/demo/jdbc/MemberRepositoryTest.java
@@ -6,12 +6,21 @@ import org.junit.jupiter.api.Test;
 
 import com.example.demo.jdbc.repository.MemberRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MemberRepositoryTest {
 
 	MemberRepository repo = new MemberRepository();
 	
-	@Test
-	void crud() throws SQLException {
+//	@Test
+	void save() throws SQLException {
 		repo.save(98, "baaa");
+	}
+
+	@Test
+	void read() throws SQLException {
+		String a = repo.findById(12L);
+		log.info(a);
 	}
 }
--- src/test/java/com/example/demo/jdbc/repository/MemberRepository.java
+++ src/test/java/com/example/demo/jdbc/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.NoSuchElementException;
 
 import com.example.demo.jdbc.connection.DBConnectionUtil;
 
@@ -35,6 +36,39 @@ public class MemberRepository {
 			close(con, prstmt, null);
 		}
 	}
+	
+	// 데이터를 변경할 때는 executeUpdate(), 데이터를 조회할 때는 executeQuery()를 사용
+	public String findById(Long id) throws SQLException {
+		String sql = "select * from member where id = ?";
+		
+		Connection con = null;
+		PreparedStatement prstmt = null;
+		ResultSet rs = null;
+		
+		con = DBConnectionUtil.getConnection();
+		
+		try {
+			prstmt = con.prepareStatement(sql);
+			prstmt.setLong(1, id);
+			
+			rs = prstmt.executeQuery();
+			
+			if (rs.next()) {
+				return rs.getString("name") + ": " + rs.getInt("age");
+			} else {
+				throw new NoSuchElementException("member not found Id="+ id);
+			}
+				
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, prstmt, rs);
+		}
+		
+	}
+	
+	
 	
 	// 리소스 정리
 	// 안해주면,커넥션이 끊어지지 않고 계속 유지되는 문제가 발생할 수 있다( 리소스 누수 )
