2eb93198df442ed836d4e7a914c1c931a75a6907
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-11-11 16:03:28 +0900

    일단 AccessTocken을 Cookie로 처리함, 아마 이거 다른걸로 나중에 바꿔야 할껄? 서버에서 Header에 토큰 실어주는거로 + 프론트에서 요청 Header에 토큰 실어주는거로, postman에서도 Cookie사용할수 있음

--- src/main/java/com/example/demo/uu/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/uu/jwt/JwtAuthenticationFilter.java
@@ -11,6 +11,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -23,9 +24,24 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
+		String token = null;
 		// 요청Header에서 JWT 토큰 추출
-		String token = resolveToken((HttpServletRequest) request);
+//		String token = resolveToken((HttpServletRequest) request);
 
+		// 일단 Cookie로 한거 , 컨트롤러 메서드 파라미터에선 간단히 @CookieValue(name="memberId", required=false) Long memberId 이런식으로 하면되는데
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		
+		Cookie[] cookies = httpRequest.getCookies(); // 모든 쿠키 가져오기
+		if (cookies != null) {
+			 for (Cookie cookie : cookies) {
+				 String name = cookie.getName(); // 쿠키 이름 가져오기
+                 if (name.equals("Authorization")) {
+                	 token = cookie.getValue(); // 쿠키 값 가져오기
+                	 break;
+                 }
+             }
+		}
+		
 		// 유효한 토큰인경우 해당 토큰의 인증 정보(Authentication)를 SecurityContext에 저장
         if (token != null && jwtTokenProvider.validateToken(token)) {
             Authentication authentication = jwtTokenProvider.getAuthentication(token);
--- src/main/java/com/example/demo/uu/jwt/MemberController.java
+++ src/main/java/com/example/demo/uu/jwt/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.demo.uu.jwt;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.security.CustomUserDetails;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,18 +24,31 @@ public class MemberController {
 	private final MemberService memberService;
 
     @PostMapping("/sign-in")
-    public JwtToken signIn(@RequestBody SignInDto signInDto) {
+    public String signIn(@RequestBody SignInDto signInDto, HttpServletResponse response) {
         String username = signInDto.getUsername();
         String password = signInDto.getPassword();
         JwtToken jwtToken = memberService.signIn(username, password);
         
         log.info("jwtToken accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
         
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.setBearerAuth(jwtToken.getAccessToken());
         
 //        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 //        CustomUserDetails userDetail = (CustomUserDetails) authentication.getPrincipal();
         
-        return jwtToken;
+        
+        /*ACCESS TOKEN 쿠키로 발급*/
+        Cookie accessCookie = new Cookie("Authorization", jwtToken.getAccessToken());
+        accessCookie.setMaxAge(60 * 10); // 10분 동안 유효
+//        accessCookie.setPath("/");
+//        accessCookie.setDomain("localhost");
+//        accessCookie.setSecure(false);
+
+        response.addCookie(accessCookie);
+        
+        
+        return "login success";
     }
 
     @PostMapping("/test")
