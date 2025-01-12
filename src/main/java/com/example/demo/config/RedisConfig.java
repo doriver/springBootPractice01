7c668266f5fbf7b6cc226acb5511913d66ec09e5
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-01-12 23:34:11 +0900

    예전에 했던 케시기능 적용시키기 시작

--- src/main/java/com/example/demo/config/RedisConfig.java
+++ src/main/java/com/example/demo/config/RedisConfig.java
@@ -1,7 +1,12 @@
 package com.example.demo.config;
 
+import java.time.Duration;
+
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -9,6 +14,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration 
+@EnableCaching
 public class RedisConfig {
 
 	@Bean
@@ -27,4 +33,23 @@ public class RedisConfig {
 		return template;
 	}
 	
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig() // static메서드 defaultCacheConfig
+                .entryTtl(Duration.ofHours(1)) // 캐시에 저장된 항목의 Time-To-Live(TTL)을 설정, 캐시 항목의 유효한 기간을 의미, 이 기간이 지나면 캐시에서 자동으로 제거
+                .disableCachingNullValues(); // null 값을 캐시에 저장하지 않도록 설정
+        
+        /*
+         * .prefixCacheNameWith(this.getClass().getPackageName() + ".") // 이거 있으면 Redis에 저장되는 key앞에 com.example.demo.config. 붙음
+         * 
+         * serializeKeysWith() : Redis에 저장되는 캐시 키를 직렬화하는 방식을 지정, serializeKeysWith()이 없으면 기본 직렬화 방식이 사용됨
+         * 
+         * .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+         * .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+         */
+        
+        return RedisCacheManager.builder(connectionFactory) // static메서드 builder 반환타입은 RedisCacheManagerBuilder 
+                .cacheDefaults(config) //
+                .build();
+    }
 }
--- src/main/java/com/example/demo/service/RedisService.java
+++ src/main/java/com/example/demo/service/RedisService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,19 @@ public class RedisService {
 
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
+	
+	
+    @Cacheable(value = "getRedis", key = "#p0")
+    public String getOnCache(String key) {      
+        int a = 0; // 디버깅용 
+        return "someData";
+    }
+
+    @Cacheable(value = "multiParam", key = "#p0 + '_' + #p1")
+    public String get22OnCache(String key, String kk) {
+        int a = 0; // 디버깅용 
+        return "multiParam someData";
+    }
 	
 	
 	/*
