a6a004bf1e6b43722de3a4c23966a1f14fb51dbf
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-04-14 16:40:46 +0900

    post요청 json데이터 자바dto로 파징 성공

--- ControllerThymeleaf/src/main/java/com/example/demo/dataBinding/BindingController.java
+++ ControllerThymeleaf/src/main/java/com/example/demo/dataBinding/BindingController.java
@@ -1,5 +1,22 @@
 package com.example.demo.dataBinding;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.RequestJsonDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/bind")
 public class BindingController {
 
+	@PostMapping("/t1")
+	public String ex01(@RequestBody RequestJsonDto dto) {
+		log.info("{} {}", dto.getName(), dto.getAge());
+		return "성공";
+	}
 }
