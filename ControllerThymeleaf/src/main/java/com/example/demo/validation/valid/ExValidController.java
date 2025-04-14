141333810b3cf18b035bc81e4d46cf910af5313d
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-04-14 11:41:10 +0900

    @Valid로 BeanValidation 테스트 해볼꺼임

---
+++ ControllerThymeleaf/src/main/java/com/example/demo/validation/valid/ExValidController.java
@@ -0,0 +1,16 @@
+package com.example.demo.validation.valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/valid")
+public class ExValidController {
+
+	@PostMapping("/t1")
+	public String ex01() {
+		
+		return "통과";
+	}
+}
