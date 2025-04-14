9aeaa181e0f2836b093d13c91fcae121211b4530
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-04-14 19:42:57 +0900

    @RequestBody @Valid 로 beanValidation구현함

--- ControllerThymeleaf/src/main/java/com/example/demo/dto/RequestJsonDto.java
+++ ControllerThymeleaf/src/main/java/com/example/demo/dto/RequestJsonDto.java
@@ -1,5 +1,7 @@
 package com.example.demo.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class RequestJsonDto {
+	@NotBlank
 	private String name;
+	
+	@Min(value=4)
 	private int age;
+	
+	@Min(value=4)
+	private int num;
 }
--- ControllerThymeleaf/src/main/java/com/example/demo/validation02/ExValidController.java
+++ ControllerThymeleaf/src/main/java/com/example/demo/validation02/ExValidController.java
@@ -1,16 +1,64 @@
 package com.example.demo.validation02;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.RequestJsonDto;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/valid")
 public class ExValidController {
 
 	@PostMapping("/t1")
-	public String ex01() {
-		
+	public String ex01(@RequestBody @Valid RequestJsonDto dto, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return validationStr(bindingResult);
+		}
 		return "통과";
 	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public String httpMessageNotReadableHandler(HttpMessageNotReadableException e) {
+		log.error("[exceptionHandler] ex", e);
+		
+        Throwable cause = e.getCause(); // 원인 확인
+        
+        if (cause instanceof InvalidFormatException) {
+            InvalidFormatException invalidFormatException = (InvalidFormatException) cause;
+            String field = invalidFormatException.getPath().get(0).getFieldName();
+            
+            return  field + "의 타입(숫자,문자 등등)이 잘못됐습니다.";
+        } 
+        return "asd";
+	}
+	
+	
+	public String validationStr(BindingResult bindingResult) {
+		StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append(fieldError.getField());
+            builder.append("(은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(", 입력된 값: ");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("\n");
+        }
+        return builder.toString();
+	}
+	
 }
